9e3771750ceba8a28e036db9f15024ef2e1b5564
nagi <nagi31904@gmail.com>
2016-05-25 13:31:26 +0800

    sil_action

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'capistrano-passenger', :group => :development
 
 gem "mysql2", :group => :production
 
+gem "kaminari"
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/welcome_indexs_controller.rb
+++ app/controllers/welcome_indexs_controller.rb
@@ -1,13 +1,30 @@
 class WelcomeIndexsController < ApplicationController
-  before_action :set_bookmark, :only => [:blog_index, :show, :edit, :update, :destroy]
-  before_action :set_post, :only => [:blog_index, :show, :edit, :update, :destroy]
-  before_action :set_info, :only => [:blog_index, :show, :edit, :update, :destroy]
-  before_action :set_comment, :only => [:blog_index, :show, :edit, :update, :destroy]
+  before_action :set_bookmark, :only => [:blog_index]
+  before_action :set_post, :only => [:blog_index]
+  before_action :set_info, :only => [:blog_index]
+  before_action :set_comment, :only => [:blog_index]
   before_action :set_work_category
 
   def blog_index
 
+    work_post_ca_webs = WorkCategory.find_by_title("web")
 
+
+    if work_post_ca_webs != nil
+
+      @work_category_id=WorkCategory.find_by_title("web").id
+      @work_post_webs=work_post_ca_webs.work_posts
+
+      @number_post = @work_post_webs.limit(9)
+      @number = @number_post.size
+      @number = 9
+      @number_v = (@number-1)/3
+
+    else
+
+    end
+
+    @work_post_first_webs = @work_post_webs[0..2]
 
 
     @infos = @infos.order(created_at: :desc).limit(6)
