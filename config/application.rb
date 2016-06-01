837395f3e7223d27f2932354fbffa78cc46a8706
nagi <nagi31904@gmail.com>
2016-06-01 23:06:34 +0800

    edit_new_2

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ Bundler.require(*Rails.groups)
 module Nagi
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w(ckeditor/*)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
